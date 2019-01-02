92f8eabe8d27dc2b9643d0a0c0f06fbfe80b2508
Supachai Suthikeeree <supano1995@gmail.com>
2019-01-02 17:48:35 +0700

    article api CRUD /api/articles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,18 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
 use Illuminate\Http\Request;
+use App\Http\Resources\Article as ArticleResource;
 
 class ArticleController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
     public function index()
     {
-        //
+        $articles = Article::paginate(15);
+
+        return ArticleResource::collection($articles);
     }
 
     /**
@@ -30,22 +34,34 @@ class ArticleController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \App\Http\Resources\Article || String
      */
     public function store(Request $request)
     {
-        //
+
+        $article = new Article();
+
+        $article->title = $request->input('article_title');
+        $article->body = $request->input('article_body');
+
+        if ($article->save()) {
+            return new ArticleResource($article);
+        }
+
+        return 'fail';
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \App\Http\Resources\Article
      */
     public function show($id)
     {
-        //
+        $article = Article::findOrFail($id);
+
+        return new ArticleResource($article);
     }
 
     /**
@@ -68,17 +84,31 @@ class ArticleController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $article = Article::findOrFail($id);
+
+        $article->id = $id;
+        $article->title = $request->input('article_title');
+        $article->body = $request->input('article_body');
+
+        if ($article->save()) {
+            return new ArticleResource($article);
+        }
+
+        return 'fail';
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \App\Http\Resources\Article
      */
     public function destroy($id)
     {
-        //
+        $article = Article::findOrFail($id);
+
+        if($article->delete()) {
+            return new ArticleResource($article);
+        }
     }
 }
