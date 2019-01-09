df62d99d69f9fc6a096ef2a379e1d0df713d1f50
Supachai Suthikeeree <supano1995@gmail.com>
2019-01-09 11:13:25 +0700

    init vue-router

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,14 +1,44 @@
-window.Vue = require('vue');
+/**
+ * import state
+ */
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import AppRoot from './pages/AppRoot'
 
+/**
+ * import Components
+ */
+import AppRoot from './pages/AppRoot'
+import Login from './pages/Login'
+
+
+
+Vue.use(VueRouter)
 Vue.use(ElementUI)
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'Main',
+            component: AppRoot
+        },
+        {
+            path: '/login',
+            name: 'Login',
+            component: Login
+        },
+    ],
+})
 
 const app = new Vue({
     el: '#app',
-    template: '<AppRoot />',
+    router,
     components: {
-        AppRoot
+        AppRoot,
+        Login
     }
-});
+})
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,9 @@
 
     </head>
     <body>
-        <div id="app"></div>
+        <div id="app">
+            <router-view></router-view>
+        </div>
 
         <script src="{{ asset('js/app.js') }}"></script>
         <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
