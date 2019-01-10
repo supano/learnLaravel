6d9daa0958cc196ca6a3a35605577530515f3544
Supachai Suthikeeree <supano1995@gmail.com>
2019-01-10 16:08:16 +0700

    add webpack alias

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,16 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.webpackConfig({
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': __dirname + '/resources/assets/js'
+        },
+    },
+})
+
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
